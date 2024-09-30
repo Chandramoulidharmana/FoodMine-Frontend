169594cb550307f20ec2547d54c60ef14f2901eb
Chandramoulidharmana <dharmanachandramouli2004@gmail.com>
2024-09-30 16:52:28 +0530

    cart page media query

--- server.ts
+++ server.ts
@@ -24,6 +24,7 @@ export function app(): express.Express {
 
 function run(): void {
   const port = process.env['PORT'] || 4000;
+  // const port = "https://food-mine-frontend.vercel.app"
 
   // Start up the Node server
   const server = app();
